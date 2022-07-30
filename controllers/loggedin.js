802ab77188171ec646dd61fc7b6bc5f10284f650
Aquos06 <ruliciohansen@gmail.com>
2022-07-30 23:36:35 +0700

    v2

---
+++ controllers/loggedin.js
@@ -0,0 +1,11 @@
+const sql = require("mysql");
+const db = sql.createConnection({
+    host:"localhost",
+    database:"online_learning",
+    user: "root",
+    password: ""
+})
+
+const loggedIn = (req, res) =>{
+
+}
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const app = express();
+const cookie = require("cookie-parser");
 const path = require('path');
+const sql = require('mysql');
+
+const db = sql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: ""
+})
+
+app.use(cookie())
+app.use(express.json())
 
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
-app.get('/', (req, res)=>{
-    res.render('home')
+app.get("/", (req,res) =>{
+    res.render("home");
+})
+app.get("/register", (req,res) => {
+    res.render("register.ejs", {root:"./views"});
+})
+app.get("/login", (req, res) =>{
+    res.render("login.ejs", {root: "./views"});
+})
+app.use("/api", require("./controllers/auth"));
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log("Database Connected..")
 })
 
 app.listen(3000,()=>{
