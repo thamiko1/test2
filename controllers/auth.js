40d27a7966fef44c5d67146ec8badd78dfa8c095
Aquos06 <ruliciohansen@gmail.com>
2022-08-03 16:08:48 +0700

    test

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const register = require("./register")
 const login = require("./login")
+const contactus = require("./contactus")
 // const logout = require("./logout")
 const router = express.Router();
 
 router.post("/register", register)
 router.post("/login", login)
+router.post("/contactus", contactus)
 // router.post("./logout", logout)
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
 const cookie = require("cookie-parser");
@@ -10,9 +11,10 @@ const db = sql.createConnection({
     password: ""
 })
 
-app.use(cookie())
-app.use(express.json())
+app.use(cookie());
+app.use(express.json());
 
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
@@ -23,10 +25,19 @@ app.get("/", (req,res) =>{
 app.get("/register", (req,res) => {
     res.render("register.ejs", {root:"./views"});
 })
+app.get("/AboutUs", (req,res) => {
+    res.render("AboutUs.ejs", {root: "./views"});
+})
 app.get("/login", (req, res) =>{
     res.render("login.ejs", {root: "./views"});
 })
+
+
+app.get("/contactus", (req,res) => {
+    res.render("ContactUs.ejs", {root:"./views"})
+})
 app.use("/api", require("./controllers/auth"));
+
 
 db.connect((err) => {
     if (err) throw err;
